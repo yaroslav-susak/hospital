451ba57998332b78c10f68adc644ca905d8151e1
Taras <tariksyurts@gmail.com>
2019-09-19 16:44:22 +0300

    add some methods to loginCont

--- src/main/java/com/itcluster/javaadvanced2/hospital/controller/UIController.java
+++ src/main/java/com/itcluster/javaadvanced2/hospital/controller/UIController.java
@@ -2,18 +2,23 @@ package com.itcluster.javaadvanced2.hospital.controller;
 
 import com.itcluster.javaadvanced2.hospital.model.Department;
 import com.itcluster.javaadvanced2.hospital.model.Doctor;
+import com.itcluster.javaadvanced2.hospital.model.User;
 import com.itcluster.javaadvanced2.hospital.service.DepartmentService;
 import com.itcluster.javaadvanced2.hospital.service.DoctorService;
+import com.itcluster.javaadvanced2.hospital.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class UIController {
+
     @Autowired
     DoctorService doctorService;
 
@@ -56,5 +61,6 @@ public class UIController {
         model.addAttribute("departments",departments);
         return "departments";
     }
-    
+
+
 }
