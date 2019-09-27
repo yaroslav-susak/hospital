6b36ebf45e385dbf6f0efc711ac909f9d9777a60
Yaroslav Susak <susak.slava@gmail.com>
2019-09-27 21:27:52 +0300

    Exceptions added

---
+++ src/main/java/com/itcluster/javaadvanced2/hospital/controller/ExceptionHandlingController.java
@@ -0,0 +1,26 @@
+package com.itcluster.javaadvanced2.hospital.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Slf4j
+public class ExceptionHandlingController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        log.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+
+        return mav;
+    }
+
+}
---
+++ src/main/java/com/itcluster/javaadvanced2/hospital/exceptions/DoctorNotFoundException.java
@@ -0,0 +1,13 @@
+package com.itcluster.javaadvanced2.hospital.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Лікаря з таким id не існує")
+@Slf4j
+public class DoctorNotFoundException extends IllegalArgumentException{
+    public DoctorNotFoundException(Long id) {
+        super("Не існує лікаря з id: " + id);
+    }
+}
