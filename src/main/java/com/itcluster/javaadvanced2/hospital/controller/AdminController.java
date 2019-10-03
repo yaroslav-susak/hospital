3ddb23dac1e5d947a0e94f2f09b28d0fccfa1036
keekvet <andrukh.lhor@gmail.com>
2019-10-03 20:03:28 +0300

    added user role change for admin

--- src/main/java/com/itcluster/javaadvanced2/hospital/controller/AdminController.java
+++ src/main/java/com/itcluster/javaadvanced2/hospital/controller/AdminController.java
@@ -1,12 +1,18 @@
 package com.itcluster.javaadvanced2.hospital.controller;
 
+import com.itcluster.javaadvanced2.hospital.dto.RoleToDeleteDTO;
+import com.itcluster.javaadvanced2.hospital.model.Role;
 import com.itcluster.javaadvanced2.hospital.model.User;
 import com.itcluster.javaadvanced2.hospital.service.AdminService;
+import com.itcluster.javaadvanced2.hospital.service.RoleService;
 import com.itcluster.javaadvanced2.hospital.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 
 @Controller
@@ -19,18 +25,55 @@ public class AdminController {
     @Autowired
     AdminService adminService;
 
+    @Autowired
+    RoleService roleService;
+
+
 
     @GetMapping("/find-user")
-    public String adminControl(){
+    public String findUser(){
         return "admincontrol";
     }
 
-    @GetMapping("/userinfo")
-    public String adminpControl(@RequestParam("email") String email, Model model){
+    @GetMapping("/user-info")
+    public String adminControl(@RequestParam("email") String email, Model model){
         User user = userService.findUserByEmail(email).get();
         model.addAttribute("foundedUser", user);
+        model.addAttribute("roleToDeleteDTO", new RoleToDeleteDTO() );
+        model.addAttribute("rolesToAdd", roleService.getAll().removeAll(user.getRoles()));
+        return "userinfo";
+    }
+
+    @DeleteMapping("/user-info")
+    public String deleteUserRole(@ModelAttribute("roleToDeleteDTO") RoleToDeleteDTO roleToDeleteDTO, Model model){
+
+        User user = userService.findUserByEmail(roleToDeleteDTO.getEmail()).get();
+        model.addAttribute("foundedUser", user);
+
+        Set<Role> roles = user.getRoles();
+        roles.remove(roleService.getByName(roleToDeleteDTO.getRole()));
+        user.setRoles(roles);
+
+        userService.createUpdate(user);
 
         return "userinfo";
     }
+
+    @PostMapping("/user-info")
+    public String updateUserRole(@ModelAttribute("roleToDeleteDTO") RoleToDeleteDTO roleToDeleteDTO, Model model){
+
+        User user = userService.findUserByEmail(roleToDeleteDTO.getEmail()).get();
+        model.addAttribute("foundedUser", user);
+
+        Set<Role> roles = user.getRoles();
+        roles.add(roleService.getByName(roleToDeleteDTO.getRole()));
+        user.setRoles(roles);
+
+        userService.createUpdate(user);
+
+        return "userinfo";
+    }
+
+
 
 }
