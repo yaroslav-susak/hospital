7e9f4216e9e24329cdcec9a965614fca604638da
Yaroslav Susak <susak.slava@gmail.com>
2019-10-05 18:52:19 +0300

    More fixes on role change page (id parameter in URL)

--- src/main/java/com/itcluster/javaadvanced2/hospital/controller/AdminController.java
+++ src/main/java/com/itcluster/javaadvanced2/hospital/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.itcluster.javaadvanced2.hospital.dto.RoleToChangeDTO;
 import com.itcluster.javaadvanced2.hospital.model.Role;
 import com.itcluster.javaadvanced2.hospital.model.User;
 import com.itcluster.javaadvanced2.hospital.service.AdminService;
+import com.itcluster.javaadvanced2.hospital.service.DoctorService;
 import com.itcluster.javaadvanced2.hospital.service.RoleService;
 import com.itcluster.javaadvanced2.hospital.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,22 +29,33 @@ public class AdminController {
     @Autowired
     RoleService roleService;
 
+    @Autowired
+    DoctorService doctorService;
+
     @ModelAttribute("user")
     public User activeUser(Authentication authentication) {
         return userService.findUserByEmail(authentication.getName()).get();
     }
 
     @GetMapping("/find-user")
-    public String findUser(){
+    public String findUser(Model model){
+        doctorService.addSearchOptions(model);
         return "admincontrol";
     }
 
+    @PostMapping("/find-user")
+    public String findUser(Model model, @RequestParam(name="email") String email){
+        User user = userService.findUserByEmail(email).orElse(null);
+        return "redirect:/admin/user-info?userId=" + user.getId();
+    }
+
     @GetMapping("/user-info")
-    public String adminControl(@RequestParam(name="email") String email, Model model){
-        User user = userService.findUserByEmail(email).get();
+    public String adminControl(@RequestParam(name="userId") Long id, Model model){
+        User user = userService.findById(id);
         model.addAttribute("foundedUser", user);
         model.addAttribute("roleToDeleteDTO", new RoleToChangeDTO() );
         model.addAttribute("rolesToAdd", roleService.getAll());
+        doctorService.addSearchOptions(model);
         return "userinfo";
     }
 
@@ -51,33 +63,27 @@ public class AdminController {
     public String deleteUserRole(@ModelAttribute("roleToChangeDTO") RoleToChangeDTO roleToChangeDTO, Model model){
 
         User user = userService.findUserByEmail(roleToChangeDTO.getEmail()).get();
-        model.addAttribute("foundedUser", user);
 
         Set<Role> roles = user.getRoles();
         roles.remove(roleService.getByName(roleToChangeDTO.getRole()));
         user.setRoles(roles);
 
         userService.createUpdate(user);
-        model.addAttribute("rolesToAdd", roleService.getAll());
 
-        return "userinfo";
+        return "redirect:/admin/user-info?userId=" + user.getId();
     }
 
     @PostMapping("/user-info")
     public String updateUserRole(@ModelAttribute("roleToChangeDTO") RoleToChangeDTO roleToChangeDTO, Model model){
 
         User user = userService.findUserByEmail(roleToChangeDTO.getEmail()).get();
-        model.addAttribute("foundedUser", user);
 
         Set<Role> roles = user.getRoles();
         roles.add(roleService.getByName(roleToChangeDTO.getRole()));
         user.setRoles(roles);
 
         userService.createUpdate(user);
-        model.addAttribute("rolesToAdd", roleService.getAll());
-        return "userinfo";
+
+        return "redirect:/admin/user-info?userId=" + user.getId();
     }
-
-
-
 }
